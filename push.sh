0b613db770ad1e74beb00debcd805ee8b4b89704
Yasuyuki Yamasaki <yasuyuki.yamasaki@finatext.com>
2019-03-07 20:27:20 +0900

    test

--- push.sh
+++ push.sh
@@ -4,3 +4,9 @@ if [ -z "$(git branch | grep '*' | grep productio)" ];then
    echo "Branch is not production"
    exit 1;
 fi
+
+if [ -n "$(git diff)" ];then
+   echo "Branch is not production!"
+   exit 1;
+fi
+"Branch is not production!!"
